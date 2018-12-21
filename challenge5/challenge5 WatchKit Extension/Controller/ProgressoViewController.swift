5b338bac924797bce3a3e26dc2b1b04d2042167c
Unknown <nicksonkley14@gmail.com>
2018-12-20 21:46:56 -0400

    Layout

--- challenge5/challenge5 WatchKit Extension/Controller/ProgressoViewController.swift
+++ challenge5/challenge5 WatchKit Extension/Controller/ProgressoViewController.swift
@@ -13,6 +13,8 @@ import Foundation
 class ProgressoViewController: WKInterfaceController {
     
     @IBOutlet var progressoLabel: WKInterfaceLabel!
+    @IBOutlet var coiso: WKInterfaceButton!
+    var timer = Timer()
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
@@ -21,6 +23,8 @@ class ProgressoViewController: WKInterfaceController {
         let paginas = contexto["3"] as! Int
         progressoLabel.setText("\(atual)/\(paginas)")
         // Configure interface objects here.
+        //animar(atual: atual, paginas: paginas)
+        animar(atual: atual, paginas: paginas)
     }
 
     override func willActivate() {
@@ -31,6 +35,17 @@ class ProgressoViewController: WKInterfaceController {
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+    }
+    
+    func animar(atual: Int, paginas: Int) {
+        animate(withDuration: 1, animations: {self.coiso.setRelativeWidth(CGFloat(atual/paginas), withAdjustment: CGFloat(0))})
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ProgressoViewController.action), userInfo: nil , repeats: false)
+    }
+    
+    @objc func action() {
+        
+        WKInterfaceController.reloadRootControllers(withNames: ["InterfaceControllerLivros"], contexts: nil)
+        
     }
 
 }
